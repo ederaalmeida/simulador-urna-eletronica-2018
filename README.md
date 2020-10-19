a92bb3209edf5eb272b6cc6c968639cbe6b45c13
Eder.Almeida <ederaalmeida82@gmail.com>
2020-10-19 13:59:26 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simulador Urna Eletrônica 2018
---
+++ etapas.js
@@ -0,0 +1,86 @@
+let etapas = [{
+        titulo: 'Deputado Federal',
+        numeros: 4,
+        candidatos: [{
+            numero: '9101',
+            nome: 'Atletismo',
+            partido: 'PEsp',
+            fotos: [{ url: '9101_atletismo.jpg', legenda: 'Deputado Federal' }]
+        }, {
+            numero: '9102',
+            nome: 'Gisnática Artística',
+            partido: 'PEsp',
+            fotos: [{ url: '9102_ginastica.jpg', legenda: 'Deputado Federal' }]
+        }, {
+            numero: '9103',
+            nome: 'Boxe',
+            partido: 'PEsp',
+            fotos: [{ url: '9103_boxe.jpg', legenda: 'Deputado Federal' }]
+        }, {
+            numero: '9104',
+            nome: 'halterofilismo',
+            partido: 'PEsp',
+            fotos: [{ url: '9104_halterofilismo.jpg', legenda: 'Deputado Federal' }]
+        }]
+    },
+    {
+        titulo: 'Deputado Estadual',
+        numeros: 5,
+        candidatos: [{
+            numero: '91001',
+            nome: 'Basquete',
+            partido: 'PEsp',
+            fotos: [{ url: '91001_basquete.jpg', legenda: 'Deputado Estadual' }]
+        }, {
+            numero: '91002',
+            nome: 'Hipismo',
+            partido: 'PEsp',
+            fotos: [{ url: '91002_hipismo.jpg', legenda: 'Deputado Estadual' }]
+        }, {
+            numero: '91003',
+            nome: 'Patinação',
+            partido: 'PEsp',
+            fotos: [{ url: '91003_patinacao.jpg', legenda: 'Deputado Estadual' }]
+        }]
+    },
+    {
+        titulo: 'Senador',
+        numeros: 3,
+        candidatos: [{
+            numero: '911',
+            nome: 'Natação',
+            partido: 'PEsp',
+            suplente_1: 'Esgrima',
+            suplente_2: 'Rugbi',
+            fotos: [{ url: '911_natacao.jpg', legenda: 'Senador', small: false },
+                { url: '911_esgrima.jpg', legenda: '1ª Suplente', small: true },
+                { url: '911_rugbi.jpg', legenda: '2ª Suplente', small: true }
+            ]
+        }]
+    },
+    {
+        titulo: 'Governador',
+        numeros: 2,
+        candidatos: [{
+            numero: '91',
+            nome: 'Volei',
+            partido: 'PEsp',
+            vice_Governador: 'Tenis',
+            fotos: [{ url: '91_volei.jpg', legenda: 'Governador', small: false },
+                { url: '91_tenis.jpg', legenda: 'Vice-Governador', small: true }
+            ]
+        }]
+    }, {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [{
+            numero: '91',
+            nome: 'Futebol',
+            partido: 'PEsp',
+            vice_Presidente: 'Judo',
+            fotos: [{ url: '91_futebol.jpg', legenda: 'Presidente', small: false },
+                { url: '91_judo.jpg', legenda: 'Vice-Presidente', small: true }
+            ]
+        }]
+    }
+];
